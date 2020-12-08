df3d4223dde9e915d8e855d28c9a8f76bc6c6c74
ayrtonaoki <ayrtonsasouza@gmail.com>
2020-12-08 17:07:52 -0300

    Create attack and special features

--- project1/app.js
+++ project1/app.js
@@ -2,7 +2,7 @@ new Vue({
     el: "#app",
     data: {
         playerHp: 100,
-        enemyHp: 0,
+        enemyHp: 100,
         running: false
     },
     computed: {
@@ -16,8 +16,23 @@ new Vue({
             this.playerHp = 100
             this.enemyHp = 100
         },
+        attack(special) {
+            this.attackPower('playerHp', 7, 12, special)
+            this.attackPower('enemyHp', 7, 12, special)
+        },
+        attackPower(target, min, max, special) {
+            const plus = special ? 5 : 0
+            const damage = this.getRandom(min + plus, max + plus)
+            this[target] = Math.max(this[target] - damage, 0)
+        },
+        getRandom(min, max) {
+            const value = Math.random() * (max - min) + min
+            return Math.round(value)
+        }
     },
     watch: {
-
+        hasResult(value) {
+            if(value) this.running = false
+        }
     }
 })
