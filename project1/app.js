1c089fc4a34c63c24309946b48279edae7224916
ayrtonaoki <ayrtonsasouza@gmail.com>
2020-12-08 18:02:36 -0300

    Create heal and logs

--- project1/app.js
+++ project1/app.js
@@ -3,7 +3,8 @@ new Vue({
     data: {
         playerHp: 100,
         enemyHp: 100,
-        running: false
+        running: false,
+        logs: []
     },
     computed: {
         hasResult() {
@@ -17,17 +18,31 @@ new Vue({
             this.enemyHp = 100
         },
         attack(special) {
-            this.attackPower('playerHp', 7, 12, special)
-            this.attackPower('enemyHp', 7, 12, special)
+            this.attackPower('enemyHp', 5, 10, special, 'Player', 'Enemy', 'good')
+            if(this.enemyHp > 0) {
+                this.attackPower('playerHp', 7, 12, false, 'Enemy', 'Player', 'bad')
+            }
         },
-        attackPower(target, min, max, special) {
+        attackPower(character, min, max, special, source, target, type) {
             const plus = special ? 5 : 0
             const damage = this.getRandom(min + plus, max + plus)
-            this[target] = Math.max(this[target] - damage, 0)
+            this[character] = Math.max(this[character] - damage, 0)
+            this.logRegister(`${source} hitted ${target}! ${damage} damage dealt`, type)
+        },
+        heal() {
+            this.healPower(10, 15)
+            this.attackPower('playerHp', 7, 12, false)
+        },
+        healPower(min, max) {
+            const heal = this.getRandom(min, max)
+            this.playerHp = Math.min(this.playerHp + heal, 100)
         },
         getRandom(min, max) {
             const value = Math.random() * (max - min) + min
             return Math.round(value)
+        },
+        logRegister(text, type) {
+            this.logs.unshift({text, type})
         }
     },
     watch: {
