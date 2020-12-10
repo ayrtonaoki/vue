216b338fd150b3120adeefa8c0247966d203b492
ayrtonaoki <ayrtonsasouza@gmail.com>
2020-12-10 18:32:23 -0300

    Set components local

---
+++ exercises2/test-project/src/Counters.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="counters">
+        <app-counter />
+        <h1>Counters</h1>
+    </div>
+</template>
+
+<script>
+
+import Counter from './Counter.vue'
+
+export default {
+    components: {
+        'app-counter': Counter
+    }
+}
+</script>
+
+<style>
+
+</style>
