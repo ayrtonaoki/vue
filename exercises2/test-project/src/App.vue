dca88b65f2ceb6374f2d48f8b557534ecf6a03ca
ayrtonaoki <ayrtonsasouza@gmail.com>
2020-12-10 18:22:15 -0300

    Start vue test-project

---
+++ exercises2/test-project/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <h1>Counters</h1>
+        <app-counter />
+        <app-counter />
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ exercises2/test-project/src/Counter.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+        <span> {{ counter }} </span>
+        <button @click="sum">+</button>
+        <button @click="subtract">-</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        sum() {
+            this.counter++
+        },
+        subtract() {
+            this.counter--
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
